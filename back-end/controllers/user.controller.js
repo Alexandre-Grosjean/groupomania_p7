2356ed24cacd237a505d5a53a5ad875c8074a850
Maki-Alex <apj.grosjean@gmail.com>
2022-03-10 23:06:05 +0100

    update & desactivation account

---
+++ back-end/controllers/user.controller.js
@@ -0,0 +1,80 @@
+const { User } = require('../models');
+const fs = require('fs');
+
+exports.profil = async (req, res) => {
+    const email = req.body.email
+    try {
+        const user = await User.findOne({
+            where: { email }
+        });
+
+        return res.json(user)
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: 'something went wrong!' })
+    }
+}
+
+exports.updateProfil = async (req, res) => {
+    const uuid = req.params.uuid;
+    const { name } = req.body;
+    const imageProfil = req.file;
+
+    try {
+        const user = await User.findOne({
+            where: { uuid }
+        });
+        if (imageProfil) {
+            if (user.imageUrl != 'http://localhost:5000/images/profils/defaultProfils.jpg') {
+                const filename = user.imageUrl.split('/profils/')[1];
+                fs.unlink(`./images/profils/${filename}`, async () => {
+
+                    user.name = name;
+                    user.imageUrl = `http://localhost:5000/images/profils/${imageProfil.filename}`;
+
+                    await user.save();
+
+                    return res.status(200).json({ message: "profil updated with image" });
+                })
+
+            } else {
+                user.name = name;
+                user.imageUrl = `http://localhost:5000/images/profils/${imageProfil.filename}`;
+
+                await user.save();
+
+                return res.status(200).json({ message: "profil updated with image" });
+            }
+
+        } else if (!imageProfil) {
+
+            user.name = name;
+            user.imageUrl = user.imageUrl;
+
+            await user.save();
+
+            return res.status(200).json({ message: "profil updated" });
+        }
+
+    } catch {
+        return res.status(500).json({ error: 'something went wrong!' })
+    }
+}
+
+exports.desactivate = async (req, res) => {
+    const uuid = req.params.uuid;
+
+    try {
+        const user = await User.findOne({
+            where: { uuid }
+        });
+
+        user.active = false;
+
+        await user.save();
+        return res.status(200).json({ message: "account desactivated" });
+
+    } catch {
+        return res.status(500).json({ error: 'something went wrong!' })
+    }
+}
--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const authCtrl = require('../controllers/auth.controller');
+const userCtrl = require('../controllers/user.controller');
 const upload = require('../middleware/multer-config');
 
 // registration
 router.post('/register', authCtrl.register);
 // login
 router.post('/login', authCtrl.login);
+// user profil
+router.get('/profil', userCtrl.profil);
+// update profil
+router.put('/updateProfil/users/:uuid', upload.single('imageProfil'), userCtrl.updateProfil);
+//desactivation account
+router.put('/desactivate/users/:uuid', userCtrl.desactivate);
 
-//test
-// router.post('/test',upload.single('imagePost'),upload.single('imageProfil'), authCtrl.test);
 
 module.exports = router;
